84cf542dce524a7864716f1469f8e2891e5a1c01
steve_siebert <steve_siebert@GORD14NWKIGD097>
2014-08-25 17:12:22 -0400

    moving crap around

--- src/main/java/org/geoint/capco/impl/policy/CountryComponentPolicy.java
+++ src/main/java/org/geoint/capco/impl/policy/CountryComponentPolicy.java
@@ -1,8 +1,9 @@
 package org.geoint.capco.impl.policy;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import org.geoint.capco.CapcoException;
 import org.geoint.capco.marking.CountryComponent;
 import org.geoint.capco.marking.SecurityMarking;
@@ -14,14 +15,35 @@ import org.geoint.capco.marking.SecurityMarking;
 public class CountryComponentPolicy<C extends CountryComponent>
         implements ComponentPolicy<C> {
 
+    private Map<String, C> components = new HashMap<>();
     //indicies
-    private Map<String, List<String>> countries = new HashMap<>(); //key=country, value=list of assoc aliases
+    private Map<String, Set<String>> countries = new HashMap<>(); //key=country, value=list of assoc aliases
     private Map<String, String[]> aliasIndex = new HashMap<>(); //key=alias, value=countries
-    
-    
+
     @Override
-    public void addComponent(C component) throws CapcoException {
+    public void addComponent(C c) throws CapcoException {
+        final String code = c.getCountry().getCode();
         
+        if (components.containsKey(code)) {
+            return;
+        }
+        components.put(code, c);
+
+        final String[] alias = c.getCountry().getAlias();
+
+        if (alias != null) {
+            //this is a country alias
+            aliasIndex.put(code, alias);
+
+            //add the country reverse index
+            for (String a : alias) {
+                getCountryAssocAliases(a).add(code);
+            }
+        } else {
+            //is not an alias
+            //creates the reference in the index if not already there
+            getCountryAssocAliases(code);
+        }
     }
 
     @Override
@@ -36,17 +58,24 @@ public class CountryComponentPolicy<C extends CountryComponent>
 
     @Override
     public boolean isComponentString(String component) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return countries.containsKey(component) || aliasIndex.containsKey(component);
     }
 
     @Override
     public C getComponent(String component) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return components.get(component);
     }
 
     @Override
     public C[] getAvailable(SecurityMarking marking) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+
+    private Set<String> getCountryAssocAliases(String code) {
+        if (!countries.containsKey(code)) {
+            countries.put(code, new HashSet<String>());
+        }
+        return countries.get(code);
     }
 
 }
